84edf48e6dfc788ee7c4928d6565b9062a3e0319
qqwxm8753000 <47600353+qqwxm8753000@users.noreply.github.com>
2022-03-29 17:09:23 +0800

    ss

--- a.py
+++ a.py
@@ -1,7 +1,8 @@
-import requests
-response = requests.get('http://www.baidu.com')print(response.status_code)  # 打印状态码
-print(response.url)          # 打印请求url
-print(response.headers)      # 打印头信息
-print(response.cookies)      # 打印cookie信息
-print(response.text)  #以文本形式打印网页源码
-print(response.content) #以字节流形式打印
+import os
+def check(strcheck:str,checks:str,output:str) :
+    if strcheck == checks :
+        output='True'
+    if strcheck == checks :
+        output='False'
+check('a','a',fla)
+print(fla)
