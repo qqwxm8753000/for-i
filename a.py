c86b8bb448f32c8cecb2bc2b02672fdc329c5946
qqwxm8753000 <47600353+qqwxm8753000@users.noreply.github.com>
2022-03-29 19:15:18 +0800

    ss

--- a.py
+++ a.py
@@ -1,8 +1,8 @@
-import os
 def check(strcheck:str,checks:str,output:str) :
-    if strcheck == checks :
-        output='True'
-    if strcheck == checks :
+    if strcheck in checks :
+        output='in'
+    if strcheck  checks :
         output='False'
+    if strcheck == checks :
+        output ='False'
 check('a','a',fla)
-print(fla)
